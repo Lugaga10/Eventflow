e8159ef95acd39c81e7a25c7b458426829b92ad6
Lugaga10 <ericlugaga9@gmail.com>
2026-04-28 12:35:02 +0300

    linking update

--- eventflow/backend/eventflow/settings.py
+++ eventflow/backend/eventflow/settings.py
@@ -157,7 +157,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok-free.app', 'eventflow-1-3lb9.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','soupier-grayce-preternaturally.ngrok-free.dev','.ngrok-free.app', 'eventflow-1-3lb9.onrender.com']
 
 DATABASES = {
     'default': env.db()
